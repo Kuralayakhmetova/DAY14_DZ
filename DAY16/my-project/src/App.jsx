f8a3e48bb712540056c7f555f3521c4d40da9bba
kuralayakhmetova <akhmetovachin@gmail.com>
2025-12-21 11:45:44 +0500

    commit 16 for ispr

---
+++ DAY16/my-project/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import styles from "./button.module.css";
+import Button from "./components/Button/Button.jsx";
+
+function App() {
+  return (
+    <div>
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ DAY16/my-project/src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import style from "./styles/button.module.scss";
+
+function Button() {
+  <div>
+    <button>Click</button>
+  </div>;
+}
+
+export default Button;
